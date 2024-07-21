67f4daae0fc210fc8dcec8398d37b985c3426d50
Michael Blesel <michael.blesel@ovgu.de>
2024-07-21 18:07:21 +0200

    Added bash completion

--- setup-env.sh
+++ setup-env.sh
@@ -2,6 +2,7 @@
 
 export SPACKTER_ROOT=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 # export PATH=${SPACKTER_ROOT}/bin:${PATH}
+. ${SPACKTER_ROOT}/bin/spackter-completions.sh
 
 spackter() {
     # echo $@ | grep -o '\-\-only\-env\-script'
